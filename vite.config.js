a83859b8ed57b41f832e8e4d50e78d38edeb7d71
Victor <vdeniskov8@gmail.com>
2025-12-03 13:37:21 +0400

    Добавлен поиск

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  build: {
-    outDir: 'docs'
-  }
+  base: './', // Относительные пути (важно!)
+  // outDir не пишем, по умолчанию это 'dist', что нам и нужно
 })
-
